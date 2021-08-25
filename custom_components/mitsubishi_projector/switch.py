c5d9e07706f83280c109c6d0757c2db07b04ebdd
dphroon <77174861+dphroon@users.noreply.github.com>
2021-08-25 13:28:54 +1000

    Update switch.py

--- custom_components/mitsubishi_projector/switch.py
+++ custom_components/mitsubishi_projector/switch.py
@@ -11,6 +11,7 @@ import voluptuous as vol
 from voluptuous.validators import Boolean
 
 from homeassistant.components.switch import PLATFORM_SCHEMA, SwitchEntity
+from homeassistant.components.select import PLATFORM_SCHEMA, SelectEntity
 from homeassistant.const import (
     CONF_FILENAME,
     CONF_NAME,
@@ -66,6 +67,7 @@ def setup_platform(
     write_timeout = config[CONF_WRITE_TIMEOUT]
 
     add_entities([MitsubishiSwitch(serial_port, name, timeout, write_timeout)], True)
+    add_entities([MitsubishiInputSelect(serial_port, name, timeout, write_timeout)], True)
 
 
 class MitsubishiSwitch(SwitchEntity):
@@ -200,3 +202,68 @@ class MitsubishiSwitch(SwitchEntity):
         msg = CMD_DICT[STATE_OFF]
         self._write_read(msg)
         self._attr_is_on = False
+
+class MitsubishiInputSelect(SelectEntity):
+
+    _attr_icon = SOURCE_ICON
+    _attr_unique_id = 'select.mitsubishi_projector_input'
+
+    def __init__(
+        self,
+        serial_port: str,
+        name: str,
+        timeout: int,
+        write_timeout: int,
+    ) -> None:
+        """Init of the Mitsubishi HC6000 projector."""
+        self.ser = serial.Serial(
+            port=serial_port, timeout=timeout, write_timeout=write_timeout,
+        )
+        self._serial_port = serial_port
+        self._attr_name = name + '_source'
+        self._attr_options = [
+            "Computer",
+            "Component",
+            "HDMI 1",
+            "HDMI 2",
+            "Video",
+            "S-Video"
+        ]
+        # self._attr_current_option = 
+
+    def _write_read(self, msg: str) -> str:
+        """Write to the projector and read the return."""
+        ret = ""
+        # Sometimes the projector won't answer for no reason or the projector
+        # was disconnected during runtime.
+        # This way the projector can be reconnected and will still work
+        try:
+            if not self.ser.is_open:
+                self.ser.open()
+            self.ser.write(msg.encode("utf-8"))
+            # Size is an experience value there is no real limit.
+            # AFAIK there is no limit and no end character so we will usually
+            # need to wait for timeout
+            ret = self.ser.read_until(size=20).decode("utf-8")
+        except serial.SerialException:
+            _LOGGER.error("Problem communicating with %s", self._serial_port)
+        self.ser.close()
+        return ret
+###
+    def _write_read_format(self, msg: str) -> str:
+        """Write msg, obtain answer and format output."""
+        # answers are formatted as answer\r
+        awns = self._write_read(msg)
+        match = re.search(r"\r(.+)\r", awns)
+        if match:
+            return match.group(1)
+        return awns #STATE_UNKNOWN
+
+    def current_option(self) -> None:
+        awns = "HDMI 1"
+        return awns
+
+    def select_option(self, selection: str) -> None:
+        msg = CMD_DICT[selection]
+        self._write_read(msg)
+        return
